632becf70cbc65c1931080c6a7f539d7cf5a258f
notjayasurya <tkjayasurya@gmail.com>
2020-10-31 17:57:38 +0530

    path and query parameters

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get('/')
 def hello_world():
     return {"Hello" :"Vanakkam"}
+
+@app.get('/component/{component_id}')  #path parameter
+def get_component(component_id:int):
+    return{"component_id":component_id}
+
+@app.get('/component/')                 # Query parameter
+def read_component(number: int, text:str):
+    return{"number": number, "text": text}
